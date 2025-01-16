b0df5ad2add1556004ce1f8904cd5f541915b9a9
Krithika S <90347901+krithi113@users.noreply.github.com>
2025-01-17 00:36:58 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from preprocess import process_data, process_features, make_predictions
 
 app = Flask(__name__)
-CORS(app)
+CORS(app,resources={r"/*":{"origins":"*"}})
 
 @app.route('/',methods = ['GET'])
 def read():
